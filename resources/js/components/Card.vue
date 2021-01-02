2b4f696ba8ed065c6bdafb8e41995c1b565a010c
Alexandr Yakovlev <leffalt@gmail.com>
2021-01-02 21:01:16 +0700

    finished prototype of board view

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-white card rounded-sm p-2 cursor-pointer text_sm hover:bg-gray-300 mb-2 mr-2 ml-2">xxx</div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+</script>
+
+<style scoped>
+.card {
+    box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+}
+</style>
---
+++ resources/js/components/CardAddButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm mb-2 mr-2 ml-2 ">Add new card</div>
+</template>
+
+<script>
+export default {
+    name: "CardAddButton"
+}
+</script>
+
+<style scoped>
+
+</style>
