fce00d1669021e1fbca46c4236a6db88a42a5ee4
Alexandr Yakovlev <leffalt@gmail.com>
2021-01-03 12:28:05 +0700

    setup&config apollo graphql-client

---
+++ resources/js/apollo.config.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import ApolloClient from "apollo-boost";
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
+
+const apolloClient = new ApolloClient({
+   uri: 'http://smartant,lara/graphql'
+});
+
+export default new VueApollo({
+   defaultClient: apolloClient
+});
