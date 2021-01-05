428a8c6221bb66e61ac0e786abf806f571401cc7
Alexandr Yakovlev <leffalt@gmail.com>
2021-01-05 17:48:04 +0700

    global & local error handler in js

--- resources/js/Login.vue
+++ resources/js/Login.vue
@@ -58,14 +58,19 @@ export default {
         }
     },
     methods: {
-        authenticate() {
-            this.$apollo.mutate({
-                mutation: Login,
-                variables: {
-                    email: this.email,
-                    password: this.password
-                }
-            });
+        async authenticate() {
+            try {
+                await this.$apollo.mutate({
+                    mutation: Login,
+                    variables: {
+                        email: this.email,
+                        password: this.password
+                    }
+                });
+            } catch (err) {
+                console.log(err);
+            }
+            console.log("Hey!");
         }
     },
 };
