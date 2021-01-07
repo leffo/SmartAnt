8d74d99e3506080dc1fcb7019336473ebfa1920b
Alexandr Yakovlev <leffalt@gmail.com>
2021-01-07 12:28:59 +0700

    add & config Vuex for control globalState

---
+++ resources/js/vuex.config.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import apollo from "./apollo.config";
+
+Vue.use(Vuex);
+
+const store = {
+    state: {},
+    mutations: {},
+    actions: {}
+};
+
+export default new Vuex.Store(store);
